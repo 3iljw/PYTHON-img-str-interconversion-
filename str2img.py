1c7f90e12aa8f713e54a7f730849c6c6b33e1bab
ji wei lee <45241362+3iljw@users.noreply.github.com>
2022-03-13 17:37:49 +0800

    Create str2img.py

---
+++ str2img.py
@@ -0,0 +1,13 @@
+import base64
+
+from PIL import Image
+from io import BytesIO
+
+from picstr import img_str
+
+byte_data = base64.b64decode(img_str)
+image_data = BytesIO(byte_data)
+
+
+byteImg = Image.open(image_data)
+byteImg.save("./export.png", "PNG")
