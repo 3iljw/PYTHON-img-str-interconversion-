9cffbca68e0cb66d2a37aab4167e0aa230f26a27
ji wei lee <45241362+3iljw@users.noreply.github.com>
2022-03-13 17:50:55 +0800

    Update str2img.py

--- str2img.py
+++ str2img.py
@@ -3,7 +3,7 @@ import base64
 from PIL import Image
 from io import BytesIO
 
-from picstr import img_str
+from picstr import img_str  # img_str is byte-string in picstr.py
 
 byte_data = base64.b64decode(img_str)
 image_data = BytesIO(byte_data)
