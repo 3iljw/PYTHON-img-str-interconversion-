0bf572fe700e62a38b46f1d6aa36db30dedba720
ji wei lee <45241362+3iljw@users.noreply.github.com>
2022-03-13 17:50:15 +0800

    Update img2str.py

--- img2str.py
+++ img2str.py
@@ -5,7 +5,7 @@ def pic2str(file, functionName):
     content = '{} = {}\n'.format(functionName, base64.b64encode(pic.read()))
     pic.close()
 
-    with open('picstr.py', 'w+') as f:
+    with open('picstr.py', 'w+') as f:  # picstr.py is output file name
         f.write(content)
 
-pic2str('./example.jpg', 'img_str')
+pic2str('./example.jpg', 'img_str')     # example.jpg is input image file and img_str is byte-string name in picstr.py
